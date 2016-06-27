84b20cfc44b1a82b0040d90cd786d9be8041d278
Tim Toupin <tim2pin@gmail.com>
2016-06-26 20:28:26 -0500

    js challenges/assignment

--- functional-practice/fp1/fp1.js
+++ functional-practice/fp1/fp1.js
@@ -5,7 +5,9 @@
 // ----------------------------
 
 function forEach(array, callback){
-    // YOUR CODE HERE
+    for (i=0; i < array.length; i++) {
+        callback(array[i], i, array);
+    }
 }
 
 // tests
@@ -20,23 +22,27 @@ console.assert(total === 24)
 // ----------------------------
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 function reduce(array, callback){
-//     var accumulator;
-//     forEach(array, function(value, i, callbackArray){}
-
-// if(i === 0){
-//     accumulator = value;
-// }else{
-//     if(i <array.length){
-        
-//     }
-// }
-// }
+  var accumulator;
+  
+  forEach(array, function(value, i, callbackArray) {
+    // If it is our first time through, use the first value
+    if(i === 0) {
+      accumulator = value;
+    }
+    // Otherwise, 
+    else if (i < array.length) {
+      accumulator = callback(accumulator, value);
+    }
+  });
+  return accumulator;
+}
 
 // tests
 // ---
 console.assert(
     reduce([1, 2, 3, 4], function(a, v){ return a*v }) === 24
-)
+);
+
 
 // ----------------------------
 // using forEach() from above, write your own map()
@@ -44,7 +50,12 @@ console.assert(
 // ----------------------------
 
 function map(array, callback){
-    // YOUR CODE HERE
+    var mapArray = [];
+    
+    forEach (array, function (array, i) {
+        mapArray[i]= callback(array);
+    });
+    return mapArray;  
 }
 
 // tests
@@ -61,15 +72,21 @@ console.assert(squares[3] === 16)
 // ----------------------------
 
 function filter(array, callback){
-    // YOUR CODE HERE
+    var filterArray = [];
+    reduce (array, function (v, i){
+    var index = callback(i);
+        if (index) {
+            filterArray.push(i);
+        }
+    });
+        return filterArray;
 }
 
 // tests
 // ---
-var evens = filter([1, 2, 3, 4], function(v){ return v%2 === 0 })
-console.assert(evens[0] === 2)
-console.assert(evens[1] === 4)
-
+var evens = filter([1, 2, 3, 4], function(v){ return v%2 === 0; });
+console.assert(evens[0] === 2);
+console.assert(evens[1] === 4);
 
 // ----------------------------
 // using reduce() from above, write your own sum()
@@ -77,9 +94,12 @@ console.assert(evens[1] === 4)
 // ----------------------------
 
 function sum(){
-    // YOUR CODE HERE
+    var args = (arguments.length === 1 ? [arguments[0]] : Array.apply(null, arguments));
+    var total = args.reduce(function(a,b){
+        return a + b;
+    });
+    return total;
 }
-
 // tests
 // ---
 console.assert(sum(1, 2, 3) === 6)
@@ -95,17 +115,22 @@ var names = [
     {name:"Matt", alma_mater:"Univ of Texas - Austin"},
     {name:"Brian", alma_mater:"Texas A&M"},
     {name:"Jesse", alma_mater:"Univ of Texas - Austin"}
-]
+];
 
-names.sort(function(a, b){
-    // YOUR CODE HERE
-})
+names.sort(function(a, b) {
+    if (a.name < b.name) {
+        return -1;
+    } else if (a.name > b.name) {
+        return 1;
+    }
+    return 0;
+});
 
 // tests
 // ---
-console.assert(names[0].name === "Brian")
-console.assert(names[1].name === "Jesse")
-console.assert(names[2].name === "Matt")
+console.assert(names[0].name === "Brian");
+console.assert(names[1].name === "Jesse");
+console.assert(names[2].name === "Matt");
 
 // ----------------------------
 // Using Array.map(), Array.filter(), and Array.sort() on the
@@ -136,4 +161,4 @@ var results = customers
 // tests
 // ---
 console.assert(results[0].fullname === "Jack White")
-console.assert(results[2].fullname === "John Smith")
+console.assert(results[2].fullname === "John Smith"
